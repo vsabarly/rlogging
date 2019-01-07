2efa30ad29dd6fffd37ea888c93913bbe02bba5a
Victor <vsabarly@digitalorigin.com>
2019-01-07 18:27:06 +0100

    Do not log stop('StopIteration') used in iterators package

--- R/stop.R
+++ R/stop.R
@@ -2,7 +2,7 @@ stop <- function(..., call.=TRUE, domain=NULL) {
 
     args <- list(...)
     is.condition <- length(args) == 1L && inherits(args[[1L]], "condition")
-    if (!is.condition) {
+    if (!is.condition & (length(args) > 0 && args[[1]] != "StopIteration")) {
         split.files <- get("split.files", envir=.rloggingOptions)
         # error messages are always printed (i.e. for levels INFO, WARN and STOP)
         if (split.files) {
